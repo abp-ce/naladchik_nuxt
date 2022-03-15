01f65a390d8fc2b78b3470b711a64d888824a935
Andrei Polyakov <ap120368@gmail.com>
2022-03-15 16:55:39 +0500

    Minor changes

--- components/Nav.vue
+++ components/Nav.vue
@@ -9,7 +9,7 @@
       <b-collapse id="navbar-toggle-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
           <b-nav-item to="offer">Услуги</b-nav-item>
-          <b-nav-item to="contracts">Договора</b-nav-item>
+          <b-nav-item to="contracts">Объекты</b-nav-item>
           <b-nav-item-dropdown text="Документы" right>
             <b-dropdown-item to="registration">Свидетельство о регистрации</b-dropdown-item>
             <b-dropdown-item to="partner_card">Карта партнёра</b-dropdown-item>
