51c57fed274abc221bb43b83a3a80fff24d6e39f
Andrei Polyakov <ap120368@gmail.com>
2022-03-16 20:47:46 +0500

    Meta description added

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { 
+        hid: 'description', 
+        name: 'description', 
+        content: 'Пуско-наладочные работы в области энергетики и автоматизации энергообъектов' 
+      },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
