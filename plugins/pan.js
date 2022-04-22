07b0b3fd9043b4127bdcba43c12cdd789a71a050
Andrei Polyakov <ap120368@gmail.com>
2022-04-22 13:20:14 +0500

    Pan to swipe

--- plugins/pan.js
+++ plugins/pan.js
@@ -13,20 +13,27 @@ export default {
   methods: {
     initHammer: function(ref) {
       var mc = new Hammer.Manager(ref)
-      mc.add(new Hammer.Pan({ direction: Hammer.DIRECTION_HORIZONTAL, threshold: 20, /*pointers: Hammer.POINTER_TOUCH*/}))
-      mc.on('panleft', this.panLeft)
-      mc.on('panright', this.panRight)
+      // mc.options.domEvents=true;
+      mc.add(new Hammer.Swipe({ direction: Hammer.DIRECTION_HORIZONTAL, /*threshold: 50, pointers: Hammer.POINTER_TOUCH*/}))
+      mc.on('swipeleft', this.swipeLeft)
+      mc.on('swiperight', this.swipeRight)
     },
-    panLeft: function(ev) {
+    swipeLeft: function(ev) {
+      // ev.srcEvent.stopPropagation()
+      // console.log(ev)
       let len = this.pages.length 
       this.currentIndex += 1
       if (this.currentIndex == len) this.currentIndex = 0
+      // console.log(this.currentIndex)
       this.$router.push({ path: this.pages[this.currentIndex], replace: true })
     },
-    panRight: function(ev) {
+    swipeRight: function(ev) {
+      // ev.srcEvent.stopPropagation()
+      // console.log(ev)
       let len = this.pages.length 
       this.currentIndex -= 1
       if (this.currentIndex == -1) this.currentIndex = len - 1
+      // console.log(this.currentIndex)
       this.$router.push({ path: this.pages[this.currentIndex], replace: true })
     }
   }
